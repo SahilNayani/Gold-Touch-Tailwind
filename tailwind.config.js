9e27fd34c646baf509642e43544c31c03434e842
Sahil <sahilnayani11@gmail.com>
2023-06-22 23:07:08 +0530

    Gold Touch

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      openSans      : ['Open-Sans', 'sans-serif'],
+      brushScript   : ['Brush-Script', 'sans-serif'],
+      courgette     : ['Courgette', 'cursive'],
+      dancingScript : ['Dancing Script', 'cursive'],
+      play          : ['Play', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'gold': '#dfb858',
+        'light-green': '#EEF2EF',
+        'light-grey': '#ECECEC',
+      },
+      fontSize: {
+        '10xl': '8.94rem',
+      },
+    },
+  },
+  plugins: [],
+}
+
