d1d072d75a5d6adfe53bdef4b4edfddf8aedbef9
Sahil <sahilnayani11@gmail.com>
2023-07-08 15:32:22 +0530

    blog

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   darkMode: 'class',
   content: ["./public/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1.25rem',
+        md: '1.75rem',
+        lg: '3.5rem',
+        xl: '5rem',
+        '2xl': '3rem',
+      },
+    },
     fontFamily: {
       openSans      : ['Open Sans', 'sans-serif'],
       courgette     : ['Courgette', 'cursive'],
